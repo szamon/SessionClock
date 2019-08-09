a86b193a1d90531151e1cd7ee49290a0de847d1a
szamon <szamon@wp.pl>
2019-08-09 16:10:42 +0200

    Updates

---
+++ precache-manifest.6744e23f5a5ceface1ed72c3fa1a086c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa261b2413a95b43e903ac26f18a470",
+    "url": "/SessionClock/index.html"
+  },
+  {
+    "revision": "efb0829c5830e5ebea9c",
+    "url": "/SessionClock/static/css/main.fcd63fc9.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/SessionClock/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "efb0829c5830e5ebea9c",
+    "url": "/SessionClock/static/js/main.9c3673dc.chunk.js"
+  },
+  {
+    "revision": "6d9a34d6ca776185c7a5",
+    "url": "/SessionClock/static/js/runtime~main.3cd9629e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SessionClock/static/media/logo.5d5d9eef.svg"
+  }
+]);
